8d66629fbd28a9defe4e95b4c75e7dbf45220c6c
Eric Marsden <eric.marsden@foncsi.org>
2024-11-25 16:21:30 +0100

    pyplayready library needs to be loaded via micropip

--- www-zola/static/js/convert.js
+++ www-zola/static/js/convert.js
@@ -19,14 +19,14 @@ const myPackages = [
     "requests",
     "/pssh-box-wasm/pyodide/construct-2.8.8-py2.py3-none-any.whl",
     "https://files.pythonhosted.org/packages/41/9f/60f8a4c8e7767a8c34f5c42428662e03fa3e38ad18ba41fcc5370ee43263/pywidevine-1.8.0-py3-none-any.whl",
-    "https://files.pythonhosted.org/packages/aa/a2/27fea39af627c0ce5dbf6108bf969ea8f5fc9376d29f11282a80e3426f1d/pymp4-1.4.0-py3-none-any.whl",
-    "pyplayready"
+    "https://files.pythonhosted.org/packages/aa/a2/27fea39af627c0ce5dbf6108bf969ea8f5fc9376d29f11282a80e3426f1d/pymp4-1.4.0-py3-none-any.whl"
 ]
 let pyodide = await loadPyodide({ packages: myPackages });
 console.log("Pyodide + pywidevine loaded");
 document.getElementById("loading").style.display = "none";
-pyodide.setDebug(true)
-micropip.install('pyplayready')
+pyodide.setDebug(true);
+micropip.install('pyplayready');
+console.log("pyplayready library loaded");
 
 const to_WVD=`
 import js
